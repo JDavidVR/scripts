91cc48f8d2a0b88c209200223ea1c665c5c9b1bf
David <JDavidVR@hotmail.com>
2017-04-19 09:51:56 -0400

    final q4os script
    Updated script and tested on x64 and x86

--- q4osWebServer.sh
+++ q4osWebServer.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 
-echo "Install openssh for remote connection";
-sudo apt-get update;
+echo 'Install openssh-server';
 sudo apt-get install openssh-server -y;
 
-echo "Install git";
+echo 'Install git-core';
 sudo apt-get install git-core -y;
+git --version;
 
-echo "Adding dotdeb.org repo";
-# install curl and add dotdeb key
+echo 'Adding repo dotdeb.gpg'
 sudo apt-get install curl -y;
-curl https://www.dotdeb.org/dotdeb.gpg | sudo apt-key add -;
-# add dotdeb repos
-sudo echo 'deb http://packages.dotdeb.org jessie all' >> /etc/apt/sources.list;
-sudo echo 'deb-src http://packages.dotdeb.org jessie all' >> /etc/apt/sources.list;
-sudo apt-get update
+curl https://www.dotdeb.org/dotdeb.gpg | sudo apt-key add -
 
-echo "install php7.0";
-sudo apt-get install php7.0 php7.0-cli php7.0-bcmath php7.0-curl php7.0-gd php7.0-intl php7.0-mbstring php7.0-mcrypt php7.0-mysql php7.0-xml php7.0-soap php7.0-xsl php7.0-zip php7.0-json php7.0-xdebug -y
+echo 'Installing PHP 7.0.x and APACHE 2.X';
+sudo su -c "echo 'deb http://packages.dotdeb.org jessie all' >> /etc/apt/sources.list"; 
+sudo su -c "echo 'deb-src http://packages.dotdeb.org jessie all' >> /etc/apt/sources.list";
+sudo apt-get update;
 
+sudo apt-get install php7.0 php7.0-cli php7.0-bcmath php7.0-curl php7.0-gd php7.0-intl php7.0-mbstring php7.0-mcrypt php7.0-mysql php7.0-xml php7.0-soap php7.0-xsl php7.0-zip php7.0-json php7.0-xdebug -y;
+sudo phpenmod xdebug;
+sudo a2enmod rewrite;
 
+echo 'Install mysql 5.7.x';
+wget https://repo.mysql.com/mysql-apt-config_0.8.4-1_all.deb;
+sudo dpkg -i mysql-apt-config_0.8.4-1_all.deb;
+sudo apt-get update;
+sudo apt-get install mysql-community-server;
+
+echo 'Install phpmyadmin';
+sudo apt-get install phpmyadmin;
+sudo cp ./phpmyadmin.conf /etc/apache2/sites-available/;
+sudo a2ensite phpmyadmin.conf;
+sudo service apache2 restart;
