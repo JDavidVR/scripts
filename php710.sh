e6c1cdea0a12db703596df08daba3bca188f7dc9
David Velasquez <david.v@wagento.com>
2017-10-20 11:16:25 -0500

    added php71 script

---
+++ php710.sh
@@ -0,0 +1,1 @@
+sudo apt-get install php7.1 php7.1-bcmath php7.1-curl php7.1-fpm php7.1-gd     php7.1-intl php7.1-mcrypt php7.1-mysql php7.1-soap php7.1-xml php7.1-zip php7.1-common php7.1-imagick -y;
