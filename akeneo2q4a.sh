a81d473a522d8f80096a36382c6b373ba68a3d7e
David <david.v@wagento.com>
2017-10-24 10:18:13 -0500

    Added node and yarn installetion steps

--- akeneo2q4a.sh
+++ akeneo2q4a.sh
@@ -52,3 +52,11 @@ echo -e " \
 \n listen.group = www-data \
 ";
 
+echo "Node";
+curl -sL https://deb.nodesource.com/setup_8.x | sudo bash -;
+sudo apt-get install -y nodejs;
+
+echo "Yarn";
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -;
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list;
+sudo apt-get update && sudo apt-get install yarn;
